a2317cc91754d35a5f198634150745de9188e23c
Avadh Dhameliya <dhameliyaavadh592@gmail.com>
2026-04-13 20:37:55 +0530

    fianl

--- toyventure-server/server.js
+++ toyventure-server/server.js
@@ -74,7 +74,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/upload', uploadRoutes);
 app.use('/api/cart', cartRoutes); 
 app.use('/api/orders', orderRoutes);
-app.use('/api/payment', paymentRoutes);
+app.use('/api/payments', paymentRoutes);
 app.use('/api/contact', contactRoutes);
 app.use('/api/coupons', couponRoutes); 
 
